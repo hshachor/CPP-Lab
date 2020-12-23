464884664e2b36177eda15fa95820bd9112405dc
Haim Shachor <hshachor@g.jct.ac.il>
2020-12-23 02:01:33 +0200

    Split headers to implementation files: Queue (and List, DoubleEndedList)

--- Queue/QueueList.cpp
+++ Queue/QueueList.cpp
@@ -1,28 +1,4 @@
-#pragma once
-#include "DoubleEndedList.h"
-#include "Queue.h"
-//-------------------------------------------------------
-//  class QueueList
-//      Queue implemented using List operations
-//-------------------------------------------------------
-class QueueList : public Queue
-{
-public:
-    // constructors
-    QueueList();
-    QueueList(const QueueList& v);
-    virtual ~QueueList() {}
-
-    // implement Queue protocol 
-    void clear() override;
-    int dequeue() override;
-    void enqueue(int value) override;
-    int front() override;
-    bool isEmpty() const override;
-
-private:
-    DoubleEndedList data;
-};
+#include "QueueList.h"
 
 //== class QueueList implementation ==
 
--- Queue/QueueVector.cpp
+++ Queue/QueueVector.cpp
@@ -1,31 +1,4 @@
-#pragma once
-#include"Queue.h"
-
-//-------------------------------------------------------
-//  class QueueVector
-//  Queue implemented using vector operations
-//-------------------------------------------------------
-class QueueVector : public Queue
-{
-public:
-    // constructor requires a starting size
-    QueueVector(int max);
-    QueueVector(const QueueVector&);
-    virtual ~QueueVector() {}
-
-    // implement Queue protocol
-    void clear() override;
-    int dequeue() override;
-    void enqueue(int value) override;
-    int front() override;
-    bool isEmpty() const override;
-
-private:
-    int * data;
-    int capacity;
-    int nextSlot;
-    int firstUse;
-};
+#include"QueueVector.h"
 
 QueueVector::QueueVector(int size)
 {
